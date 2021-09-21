e6da0c4d5f8efe3aceccdedb9abd7adff4a63aad
luke-dinh <dinhphuoclockt@gmail.com>
2021-09-21 21:29:41 +0700

    update app.py

--- api.py
+++ api.py
@@ -7,3 +7,6 @@ app = Flask(__name__)
 def score():
     features = request.json['X']
     return make_response(jsonify({'score': features}))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
