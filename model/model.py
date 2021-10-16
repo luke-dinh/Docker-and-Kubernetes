28d645764dcc74788f980723ad7a0e3ea89aac20
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-16 22:16:40 +0700

    Update all

--- model/model.py
+++ model/model.py
@@ -1,14 +1,15 @@
-import numpy as np
-import keras 
+import os 
+import keras
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPool2D, InputLayer, BatchNormalization
 from keras.layers import Dense, Flatten, Dropout
-from keras.datasets import mnist
+from keras.datasets import fashion_mnist
 import argparse
 
 parser = argparse.ArgumentParser("Model Serving")
 parser.add_argument("--input_shape", default=(28, 28, 1), type=list, help="Define input shape of model")
-parser.add_argument("--save_path", default = "model/", type=str, help="Define path to save model")
+parser.add_argument("--save_path", default = "./serving/", type=str, help="Define path to save model")
 parser.add_argument("--batch_size", default=64, type=int, help="Batch Size")
 parser.add_argument("--num_epochs", default=20, type=int, help="Number of epochs")
 opt = parser.parse_args()
@@ -51,24 +52,43 @@ ex_model = model(input_shape=input_shape)
 
 # Dataset
 num_classes = 10
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 x_train = x_train.astype('float32') / 255.0
-x_train = np.expand_dims(x_train, -1)
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.astype('float32') / 255.0
-x_test = np.expand_dims(x_test, -1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 # Train and save weight
+e_s = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
 ex_model.compile(optimizer='adam', loss="categorical_crossentropy", metrics=["accuracy"])
 
-ex_model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, verbose=1, validation_split=0.2)
-
-ex_model.save(path + "/model.h5")
+ex_model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, verbose=1, validation_split=0.2, callbacks=[e_s])
 
 # Evaluate
 score = ex_model.evaluate(x_test, y_test, verbose=0)
 print("Test loss: ", score[0])
 print("Test acc: ", score[1])
+
+# Save model for serving
+if not os.path.exists(path):
+    os.makedirs(path)
+
+version = "v1"
+export_path = os.path.join(path, version)
+print('export_path = {}\n'.format(export_path))
+
+tf.keras.models.save_model( 
+    ex_model, 
+    export_path,
+    overwrite=True,
+    include_optimizer=True,
+    save_format=None,
+    signatures=None,
+    options=None
+)
+
+print("Model Saved!")
