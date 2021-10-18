3640822521f1058ed4684b9a82af45213f0305f1
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-18 17:19:22 +0700

    Update

--- api_request.py
+++ api_request.py
@@ -2,15 +2,25 @@ import json, requests
 import numpy as np
 import keras
 from keras.datasets import fashion_mnist
+import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, f1_score
 
 # Load test set
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 x_test = x_test.astype('float32') / 255.0
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
 y_test = keras.utils.to_categorical(y_test, num_classes=10)
+
+def show(idx, title):
+  plt.figure()
+  plt.imshow(x_test[idx].reshape(28,28))
+  plt.axis('off')
+  plt.title('\n\n{}'.format(title), fontdict={'size': 16})
 
 def res_infer( 
     imgs,
@@ -25,7 +35,7 @@ def res_infer(
 
     data = json.dumps({ 
         "signature_name": signature_name,
-        "instances": imgs.tolist()
+        "instances": imgs[0:3].tolist()
     })
 
     header = {"content-type": "application/json"}
@@ -38,14 +48,14 @@ def res_infer(
 
     if json_response.status_code == 200:
         y_pred = json.loads(json_response.text)['predictions']
-        y_pred = np.argmax(y_pred, axis=-1)
         return y_pred
-    else:
-        return None
 
-y_pred = res_infer(x_test)
-acc_score = accuracy_score(np.argmax(y_test, axis=-1), y_pred)
-f1 = f1_score(np.argmax(y_test, axis=-1), y_pred, average="macro")
+result = res_infer(x_test)
+show(0, 'The model thought this was a {} (class {}), and it was actually a {} (class {})'.format(
+        class_names[np.argmax(result[0])], np.argmax(result[0]), class_names[y_test[0]], y_test[0]))
 
-print("Accuracy Score: ", acc_score)
-print("F1 Score: ", f1)
+# acc_score = accuracy_score(np.argmax(y_test, axis=-1), y_pred)
+# f1 = f1_score(np.argmax(y_test, axis=-1), y_pred, average="macro")
+
+# print("Accuracy Score: ", acc_score)
+# print("F1 Score: ", f1)
