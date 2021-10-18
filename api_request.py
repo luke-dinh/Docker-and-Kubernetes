88e6aba39c5d267f56069d9386830afa5a88c9fa
luke-dinh <dinhphuoclockt@gmail.com>
2021-10-18 16:24:30 +0700

    Update

--- api_request.py
+++ api_request.py
@@ -1,4 +1,5 @@
 import json, requests
+import numpy as np
 import keras
 from keras.datasets import fashion_mnist
 from sklearn.metrics import accuracy_score, f1_score
@@ -11,3 +12,20 @@ x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
 y_test = keras.utils.to_categorical(y_test, num_classes=10)
 
+def res_infer( 
+    imgs,
+    model_name='mnist-serving',
+    host='localhost',
+    port=8501,
+    signature_name="serving_default"
+):
+
+    if imgs.ndim==3:
+        imgs = np.expand_dims(imgs, axis=0)
+
+    data = json.dumps({ 
+        "signature_name": signature_name,
+        "instances": imgs.tolist()
+    })
+
+    header = {"content-type": "application/json"}
